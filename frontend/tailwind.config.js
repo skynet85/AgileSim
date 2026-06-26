e93d61026527291ae92e51dabab159b3e73913ca
skynet85 <l2ahmarket@gmail.com>
2026-06-26 14:19:15 +0200

    Delete frontend directory

--- frontend/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-// File: frontend/tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
-  plugins: []
-}
