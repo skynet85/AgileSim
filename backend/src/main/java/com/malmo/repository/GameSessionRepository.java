0eca5d55251af973b17e02e66a874b88ee5042e1
skynet85 <l2ahmarket@gmail.com>
2026-07-21 07:58:22 +0200

    Delete backend directory

--- backend/src/main/java/com/malmo/repository/GameSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.malmo.repository;
-
-import com.malmo.entity.GameSessionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface GameSessionRepository extends JpaRepository<GameSessionEntity, String> {
-    Optional<GameSessionEntity> findById(String id);
-}
